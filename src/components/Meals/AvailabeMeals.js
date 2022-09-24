d25dc2ba568ab98bbc9345782c7b86f476cead9e
ahmedsamir122 <eng.ahmed.elmitwalli@gmail.com>
2022-09-24 23:00:17 +0200

    test

--- src/components/Meals/AvailabeMeals.js
+++ src/components/Meals/AvailabeMeals.js
@@ -33,9 +33,7 @@ const AvailableMeals = props => {
     const mealList = DUMMY_MEALS.map(meal => <MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price ={meal.price}/>);
     return <section className={classes.meals}>
         <Card>
-            <ul>
-                {mealList}
-            </ul>
+            
         </Card>
     </section>
 }
